8197f523ac1533b83c4d5f2ecd7982496d830d8b
Juani <gez.schegtel_ji@proton.me>
2025-06-16 19:17:15 -0300

    TG-4 -progress

--- solutions/day_10/lev3.py
+++ solutions/day_10/lev3.py
@@ -1,7 +1,8 @@
 from countries import COUNTRIES
+from countries_data import COUNTRIES_DATA
 
 
-def d10_level3():
+def d10_level3() -> None:
     print("\nEstos son los ejercicios del día 10, nivel 3.\n")
 
     # ==============================
@@ -42,12 +43,26 @@ def d10_level3():
     for fruit in fruit_list[::-1]:
         print(f"{fruit}, ", end=" ")
 
-    print()
+    print("\n")
 
     # 3. Go to the data folder and use the countries_data.py file.
     #    1. What are the total number of languages in the data
     #    2. Find the ten most spoken languages from the data
     #    3. Find the 10 most populated countries in the world
+
+    print("Punto 2:")
+
+    total_lang = (
+        set()
+    )  # Al trabajar con un conjunto (set) me aseguro de no contar elementos repetidos.
+
+    for dictionary in COUNTRIES_DATA:
+        total_lang.update(dictionary.get("languages"))
+
+    print(
+        f"Los lenguajes que hay en la base de datos son: {', '.join(map(str, sorted(total_lang)))}."
+    )
+    print(f"\nRepresentan un total de {len(total_lang)} idiomas diferentes.\n")
 
 
 if __name__ == "__main__":
